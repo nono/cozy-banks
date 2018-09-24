8ec5f2ab7a7eea9090034d19a4127ac0d59cd7f5
Patrick Browne <pt.browne@gmail.com>
2018-09-24 09:34:10 +0200

    refactor: Remove cozy-ui stylus rules

--- config/webpack.config.cozy-ui.js
+++ config/webpack.config.cozy-ui.js
@@ -7,30 +7,13 @@ const path = require('path')
 
 module.exports = {
   resolve: {
-    extensions: ['.styl']
+    extensions: ['.styl'],
+    alias: {
+      'cozy-ui/react': 'cozy-ui/dist/react'
+    }
   },
   module: {
-    rules: [
-      {
-        include: path.dirname(require.resolve('cozy-ui')),
-        test: /\.styl$/,
-        loader: extractor.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader?importLoaders=2&modules&localIdentName=[name]_[local]_[hash:base64:5]',
-            'stylus-loader'
-          ]
-        })
-      },
-      {
-        test: /\.jsx?$/,
-        include: path.dirname(require.resolve('cozy-ui')),
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true
-        }
-      }
-    ]
+    rules: []
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
